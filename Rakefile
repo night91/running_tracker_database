e137143e2daa8036cee496a318113808f072b6e2
Eddy Roberto Morales Perez <eddyr.morales@gmail.com>
2017-08-13 16:42:33 +0000

    Add support for running bundle exec rake db:migrations

--- Rakefile
+++ Rakefile
@@ -4,3 +4,37 @@ require "rspec/core/rake_task"
 RSpec::Core::RakeTask.new(:spec)
 
 task :default => :spec
+
+namespace :db do
+  desc 'Run database migrations'
+  task :migrations do
+  	require 'colorize'
+    require 'highline'
+    require './lib/running_tracker_database'
+
+    begin
+      cli = HighLine.new
+      convert = -> (str) { str =~ /^y$/i }
+      message = "Are you trying to run migrations for the running tracker database ? (y / n)"
+      agree = cli.ask(message, convert) { |q| q.validate = /^(y|n)$/i }
+
+      if agree
+      	endpoint = cli.ask('Endpoint: ') { |q| q.default = "mysql2://root:root@localhost/running_tracker" }
+
+        puts 'Connecting...'.blue
+        db = RunningTrackerDatabase.connect({ 'endpoint' => endpoint, 'max_connections' => 1 })
+
+        puts 'Running migrations...'.blue
+        RunningTrackerDatabase.create_models!(db)
+
+        puts 'Done!!'.green
+        puts "Schema version number is: #{db[:schema_info].first[:database_version]}"
+      else
+        puts 'Bye'.green
+      end
+    rescue => e
+      puts "ERROR: #{e.inspect}".red
+      puts "In: #{e.backtrace.join("\n    ")}"
+    end
+  end
+end
--- models/migrations/001_init_database.rb
+++ models/migrations/001_init_database.rb
@@ -5,23 +5,26 @@ Sequel.migration do
 
     create_table(:user) do
       String      :user_id, size: 24, primary_key: true
-      String      :email, size: 50, null: false
+      String      :email, size: 50, null: false, unique: true, index: true
       String      :name, size: 50, null: false
       String      :lastname, size: 50, null: false
     end
 
     create_table(:running_session) do
-      String      :user_id, size: 24, primary_key: true
-      String      :training_session_id, size: 24, primary_key: true
-      DateTime    :start_date, null: false
+      Integer     :running_session_id, primary_key: true, auto_increment: true
+      String      :training_session_id, size: 24, index: true
+
+      DateTime    :start_date
       DateTime    :end_date
       Float       :distance
       Integer     :time_elapsed
     end
 
-    create_table(:trainning_session) do
-      String      :user_id, size: 24, primary_key: true
-      String      :training_session_id, size: 24, primary_key: true
+    create_table(:user_training_session) do
+      String      :user_id, size: 24
+      String      :training_session_id, size: 24
+
+      primary_key [:user_id, :training_session_id]
     end
   end
 end
