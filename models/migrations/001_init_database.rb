01cd3130b3c8c825058320683102f492ab670a94
Eddy Roberto Morales Perez <eddyr.morales@gmail.com>
2017-08-24 23:34:28 +0000

    Create data access objects

--- models/migrations/001_init_database.rb
+++ models/migrations/001_init_database.rb
@@ -8,23 +8,38 @@ Sequel.migration do
       String      :email, size: 50, null: false, unique: true, index: true
       String      :name, size: 50, null: false
       String      :lastname, size: 50, null: false
+      DateTime    :birthdate, null: false
+      Float       :weight
+      Float       :height
+    end
+
+    create_table(:user_stats) do
+      String      :user_id, size: 24, primary_key: true
+      String      :user_stats, size: 20, null: false, index: true
+      Float       :value, null: false
+    end
+
+    create_table(:user_activity) do
+      String      :user_id, size: 24, primary_key: true
+      String      :active_training_session_id, size: 24, null: false, index: true
+    end
+
+    create_table(:training_session) do
+      String      :training_session_id, size: 24, primary_key: true
+      String      :user_id, size: 24, index: true
+
+      DateTime    :start_time, null: false, default: Time.now
+      DateTime    :end_time
+      String      :active_session_id, size: 24, index: true
     end
 
     create_table(:running_session) do
       Integer     :running_session_id, primary_key: true, auto_increment: true
       String      :training_session_id, size: 24, index: true
 
-      DateTime    :start_date
-      DateTime    :end_date
-      Float       :distance
-      Integer     :time_elapsed
-    end
-
-    create_table(:user_training_session) do
-      String      :user_id, size: 24
-      String      :training_session_id, size: 24
-
-      primary_key [:user_id, :training_session_id]
+      DateTime    :start_time, null: false, default: Time.now
+      DateTime    :end_time
+      Float       :distance, default: 0
     end
   end
 end
